5d23483cfbe098f2f33b6dfd821aeb18f70fd980
Dan Rusei <dan.rusei@gmail.com>
2024-03-20 21:42:35 +0200

    working on posting part

--- gobot.go
+++ gobot.go
@@ -9,12 +9,13 @@ import (
 	"github.com/bluesky-social/indigo/xrpc"
 )
 
-const defaultServer = "https://bsky.social"
+const defaultPDS = "https://bsky.social"
 
 // const resolveURL = "https://bsky.social/xrpc/com.atproto.identity.resolveHandle"
 // const sessionURL = "https://bsky.social/xrpc/com.atproto.server.createSession"
 // const postFeedURL = "https://bsky.social/xrpc/com.atproto.repo.createRecord"
 
+// Wrapper over the atproto xrpc transport
 type BskyAgent struct {
 	// xrpc transport, a wrapper around http server
 	client *xrpc.Client
@@ -22,10 +23,11 @@ type BskyAgent struct {
 	apikey string
 }
 
+// Creates new BlueSky Agent
 func NewAgent(ctx context.Context, server string, handle string, apikey string) BskyAgent {
 
 	if server == "" {
-		server = defaultServer
+		server = defaultPDS
 	}
 
 	return BskyAgent{
@@ -39,26 +41,54 @@ func NewAgent(ctx context.Context, server string, handle string, apikey string) 
 
 }
 
+// Connect and Authenticate to the provided Personal Data Server, default is Bluesky PDS
+// No need to refresh the access token if the bot script will be executed based on the cron job
 func (c *BskyAgent) Connect(ctx context.Context) error {
-	// Authenticate to the Bluesky server
-	sess, err := atproto.ServerCreateSession(ctx, c.client, &atproto.ServerCreateSession_Input{
+	// Authenticate with the Bluesky server
+
+	input_for_session := &atproto.ServerCreateSession_Input{
 		Identifier: c.handle,
 		Password:   c.apikey,
-	})
+	}
+
+	session, err := atproto.ServerCreateSession(ctx, c.client, input_for_session)
 
 	if err != nil {
 		return fmt.Errorf("UNABLE TO CONNECT: %v", err)
 	}
 
-	// Construct the authenticated client and the JWT expiration metadata
+	// Access Token is used to make authenticated requests
+	// Refresh Token allows to generate a new Access Token
 	c.client.Auth = &xrpc.AuthInfo{
-		AccessJwt:  sess.AccessJwt,
-		RefreshJwt: sess.RefreshJwt,
-		Handle:     sess.Handle,
-		Did:        sess.Did,
+		AccessJwt:  session.AccessJwt,
+		RefreshJwt: session.RefreshJwt,
+		Handle:     session.Handle,
+		Did:        session.Did,
 	}
 
 	return nil
+}
+
+func (c *BskyAgent) PostToFeed(ctx context.Context) error {
+
+	post_input := &atproto.RepoCreateRecord_Input{
+		// collection: The NSID of the record collection.
+		Collection:
+		// record: The record itself. Must contain a $type field.
+		Record:
+		// repo: The handle or DID of the repo (aka, current account).
+		Repo: c.client.Auth.Did
+		// rkey: The Record Key.
+		Rkey: 
+		// swapCommit: Compare and swap with the previous commit by CID.
+		SwapCommit: 
+		// validate: Can be set to 'false' to skip Lexicon schema validation of record data.
+		Validate: true
+	}
+	
+	
+	response, err := atproto.RepoCreateRecord(ctx, c.client, post_input)
+
 }
 
 // type ResolveResponse struct {
